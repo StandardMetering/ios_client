4212765a7fb0a30d31c5f03ee3f96b427d4c2343
grbcp5 <grbcp5@mst.edu>
2018-05-15 16:16:56 -0500

    Initial Commit

---
+++ StandardWater iOS client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StandardWater iOS client
+//
+//  Created by Grant Broadwater on 5/15/18.
+//  Copyright © 2018 StandardWater. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
